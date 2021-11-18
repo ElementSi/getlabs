3d3b9a82a0438a3c5a41c83edb7b71c143fd94cc
vasdimochka <90830573+vasdimochka@users.noreply.github.com>
2021-11-18 09:17:45 +0300

    Add files via upload

--- light/scripts/lightMeasure.py
+++ light/scripts/lightMeasure.py
@@ -2,9 +2,13 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
+camera.resolution = (2560, 1440)
+camera.rotation = 180
 
 def take_photo(name):
     camera.start_preview()
-    time.sleep(5)
-    camera.capture("\\light\data\\" + name + ".jpeg")
+    sleep(5)
+    camera.capture("/home/gr106/Desktop/lab3/light-main/data/" + name +".png")
     camera.stop_preview()
+
+take_photo()
